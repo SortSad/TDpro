0e4268d5e4fadddead4f4dfe6684d5dded31b425
SortSad <97121032+SortSad@users.noreply.github.com>
2024-05-24 20:09:43 +0700

    Update teasend.lua

--- teasend.lua
+++ teasend.lua
@@ -1,5 +1,5 @@
 getgenv().EquipMacroTroop = true
-getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1080869432185856124/1236542511179104287/loisprite.json?ex=66386374&is=663711f4&hm=43f4f47ec0176152926855907b37a7d11a08be7310e7270086a17f370da1a582&"
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/797998461735403551/1243550336153550938/Mimix.json?ex=6651e200&is=66509080&hm=01765c4bd49417e4323ee7c85e3bc7d6e19545f54a8a80f55461f91a16984eef&"
 getgenv().Config = {
     ["AutoRejoin"] = true,
     ["AutoJoinMatch"] = true,
@@ -7,17 +7,17 @@ getgenv().Config = {
     ["AutoReplay"] = true,
 	["DelayReplay"] = 5,
     ["SelectMap"] = "ToiletTown",
-    ["SelectMacro"] = "loisprite",
+    ["SelectMacro"] = "Mimix",
     ["GameSpeed"] = "2",
     ["AutoVoteDifficulty"] = "true",
-    ["SelectDifficulty"] = "Hard",
+    ["SelectDifficulty"] = "Insane",
     ["AutoReplay"] = true,
     ["AutoSkip"] = true,
     ["PlayMacro"] = true,
     ["SetGameSpeed"] = true,
     ["AutoSave"] = true,
     ["BoostFPS"] = true,
-    ["BlackScreen"] = false,
+    ["BlackScreen"] = true,
     ["IgnoreMacroTiming"] = true,
 }
 getgenv().Key = "ke6982796efa8c52a555e7d9"
