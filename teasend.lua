646118b5c5007ebcf79f7abdca4771dbcf18ea7c
SortSad <97121032+SortSad@users.noreply.github.com>
2024-06-03 10:57:34 +0700

    Update teasend.lua

--- teasend.lua
+++ teasend.lua
@@ -1,25 +1,36 @@
-getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/797998461735403551/1243550336153550938/Mimix.json?ex=665c6e00&is=665b1c80&hm=095aebb2e1cba3b91a9596bea36722bf465590b6af995755d3f773c8b50c9983&"
 getgenv().EquipMacroTroop = true
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1080869432185856124/1245978790539296860/farminV2.json?ex=665ab7ac&is=6659662c&hm=fd43c6009dc2364147c67d467a48025e24d3381466ec24b422ccc9d13f169c06&"
 getgenv().Config = {
-    ["AutoVoteDifficulty"] = true,
-    ["AutoSave"] = true,
-    ["AutoReplay"] = true,
-    ["DelayReplay"] = 3,
-    ["SellWave"] = 1,
-    ["AutoSkip"] = true,
-    ["SetGameSpeed"] = true,
-    ["AutoJoinMatch"] = true,
-    ["TPLobbyIfPlayer"] = true,
-    ["GameSpeed"] = 2,
-    ["AutoSellOW"] = false,
-    ["IgnoreMacroTiming"] = true,
-    ["SelectMacro"] = "Mimix",
-    ["AutoJoinEndless"] = false,
-    ["PlayMacro"] = true,
-    ["BlackScreen"] = false,
-    ["BoostFPS"] = true,
-    ["AutoRejoin"] = true,
-    ["SelectDifficulty"] = "Insane"
+	["SelectChapter"] = "Infinite",
+	["AutoSave"] = true,
+	["BlackScreen"] = false,
+	["AutoReturnLobby"] = true,
+	["WH_MatchComplete"] = true,
+	["AutoSkip"] = true,
+	["SelectWorld"] = "Windmill Village",
+	["TPLobbyIfPlayer"] = true,
+	["IgnoreMacroTiming"] = true,
+	["SelectMacro"] = "farminV2",
+	["AutoJoinChallenge"] = false,
+	["HardMode"] = false,
+	["LeaveSellWave"] = 29,
+	["SelectReward"] = {
+		["Trait Crystal"] = false,
+		["Gold"] = false,
+		["Star Rift (Rainbow)"] = false,
+		["Gems"] = false,
+		["Risky Dice"] = false,
+		["Star Rift "] = false
+	},
+	["AutoRejoin"] = true,
+	["WebhookURL"] = "https://discord.com/api/webhooks/1245451207024316437/HnzQUc6nd82fiC-GNM7xxEx-yyqgRxGbQOzMYdy3vFhUYcOSeOUGH62Opgy1J92ndC4z",
+	["AutoSell"] = false,
+	["AutoJoinWorld"] = true,
+	["PlayMacro"] = true,
+	["BoostFPS"] = true,
+	["AutoReplay"] = false,
+        ["AutoClaimQuest"] = true,
+	["AutoLeave"] = false
 }
 getgenv().Key = "kd2415278d2a026df7ecadb4"
 repeat wait() until game:IsLoaded()
