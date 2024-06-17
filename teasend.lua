048479f36587122be94f0f6ee8bd0bcf4cc16f06
SortSad <97121032+SortSad@users.noreply.github.com>
2024-06-17 19:18:39 +0700

    Update teasend.lua

--- teasend.lua
+++ teasend.lua
@@ -1,2 +1,53 @@
+getgenv().Config = {
+	["SelectChapter"] = "Infinite",
+        ["AutoSave"] = true,
+	["WH_MatchComplete"] = false,
+	["AutoSkip"] = true,
+	["AutoClaimQuest"] = true,
+	["TPLobbyIfPlayer"] = false,
+
+	["BoostFPS"] = true,
+	["JoinerCooldown"] = 0,
+	["AutoSell"] = false,
+	["IgnoreMacroTiming"] = false,
+	["BlackScreen"] = true,
+	["AutoReturnLobby"] = true,
+	["AutoRejoin"] = true,
+	["WH_BannerSummoned"] = false,
+        ["SelectWorld"] = "Windmill Village",
+	["OFARecord"] = false,
+
+	["PlayMacro"] = false,
+	["HillHeight"] = {
+		["Cursed Academy"] = 2,
+		["Blue Planet"] = 2,
+		["Snowy Woods"] = 2,
+		["Underwater Temple"] = 2,
+		["Swordsman Dojo"] = 2,
+		["Mob City"] = 2,
+		["Windmill Village"] = 2
+	},
+	["PlaceGap"] = {
+		["Cursed Academy"] = 3.25,
+		["Blue Planet"] = 3.25,
+		["Snowy Woods"] = 3.25,
+		["Underwater Temple"] = 3.25,
+		["Swordsman Dojo"] = 3.25,
+		["Mob City"] = 3.25,
+		["Windmill Village"] = 3.25
+	},
+	["AutoJoinWorld"] = true,
+	["AutoUpgrade"] = true,
+	["UpgradeAndPlace"] = true,
+	["AutoReplay"] = false,
+	["JoinHighest"] = false,
+	["AutoPlay"] = true,
+	["DeleteMap"] = true,
+	["LeaveSellWave"] = 31,
+	["HardMode"] = false,
+	["AutoLeave"] = true,
+	["AutoJoinChallenge"] = true,
+	["AutoSummonStandard"] = false
+}
 getgenv().Key = "kd2415278d2a026df7ecadb4"
 loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
