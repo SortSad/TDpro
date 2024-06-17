5da56d7b786253bda1f898ef2950d63c2c812881
SortSad <97121032+SortSad@users.noreply.github.com>
2024-06-18 00:19:56 +0700

    Update teasend.lua

--- teasend.lua
+++ teasend.lua
@@ -1,53 +1,27 @@
+getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/797998461735403551/1243550336153550938/Mimix.json?ex=665c6e00&is=665b1c80&hm=095aebb2e1cba3b91a9596bea36722bf465590b6af995755d3f773c8b50c9983&"
+getgenv().EquipMacroTroop = true
 getgenv().Config = {
-	["SelectChapter"] = "Infinite",
-        ["AutoSave"] = true,
-	["WH_MatchComplete"] = false,
-	["AutoSkip"] = true,
-	["AutoClaimQuest"] = true,
-	["TPLobbyIfPlayer"] = false,
-
-	["BoostFPS"] = true,
-	["JoinerCooldown"] = 3,
-	["AutoSell"] = false,
-	["IgnoreMacroTiming"] = false,
-	["BlackScreen"] = true,
-	["AutoReturnLobby"] = true,
-	["AutoRejoin"] = true,
-	["WH_BannerSummoned"] = false,
-        ["SelectWorld"] = "Windmill Village",
-	["OFARecord"] = false,
-
-	["PlayMacro"] = false,
-	["HillHeight"] = {
-		["Cursed Academy"] = 2,
-		["Blue Planet"] = 2,
-		["Snowy Woods"] = 2,
-		["Underwater Temple"] = 2,
-		["Swordsman Dojo"] = 2,
-		["Mob City"] = 2,
-		["Windmill Village"] = 2
-	},
-	["PlaceGap"] = {
-		["Cursed Academy"] = 3.25,
-		["Blue Planet"] = 3.25,
-		["Snowy Woods"] = 3.25,
-		["Underwater Temple"] = 3.25,
-		["Swordsman Dojo"] = 3.25,
-		["Mob City"] = 3.25,
-		["Windmill Village"] = 3.25
-	},
-	["AutoJoinWorld"] = true,
-	["AutoUpgrade"] = true,
-	["UpgradeAndPlace"] = true,
-	["AutoReplay"] = false,
-	["JoinHighest"] = false,
-	["AutoPlay"] = true,
-	["DeleteMap"] = true,
-	["LeaveSellWave"] = 31,
-	["HardMode"] = false,
-	["AutoLeave"] = true,
-	["AutoJoinChallenge"] = true,
-	["AutoSummonStandard"] = false
+    ["AutoVoteDifficulty"] = true,
+    ["AutoSave"] = true,
+    ["AutoReplay"] = true,
+    ["DelayReplay"] = 3,
+    ["SellWave"] = 1,
+    ["AutoSkip"] = true,
+    ["SetGameSpeed"] = true,
+    ["AutoJoinMatch"] = true,
+    ["TPLobbyIfPlayer"] = true,
+    ["GameSpeed"] = 2,
+    ["AutoSellOW"] = false,
+    ["IgnoreMacroTiming"] = true,
+    ["SelectMacro"] = "Mimix",
+    ["AutoJoinEndless"] = false,
+    ["PlayMacro"] = true,
+    ["SelectMap"] = "ToiletTown",
+    ["BlackScreen"] = false,
+    ["BoostFPS"] = true,
+    ["AutoRejoin"] = true,
+    ["SelectDifficulty"] = "Insane"
 }
 getgenv().Key = "kd2415278d2a026df7ecadb4"
+repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
