41fc612d7a9978d65c5aea95023e905fd3f86831
SortSad <97121032+SortSad@users.noreply.github.com>
2024-06-22 11:03:47 +0700

    Update teasend.lua

--- teasend.lua
+++ teasend.lua
@@ -1,8 +1,5 @@
 getgenv().ImportMacro = "https://cdn.discordapp.com/attachments/1080869432185856124/1253921776157851730/Mimix.json?ex=66779d28&is=66764ba8&hm=daa762d38ed94a262d5c4814681b1fe1bfc45e5b8b7f7b6b992b3b1490454a68&"
 getgenv().EquipMacroTroop = true
-getgenv().Key = "kd2415278d2a026df7ecadb4"
-repeat wait() until game:IsLoaded()
-loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
 
 getgenv().Config = {
     ["AutoVoteDifficulty"] = true,
@@ -26,3 +23,6 @@ getgenv().Config = {
     ["AutoRejoin"] = true,
     ["SelectDifficulty"] = "Insane"
 }
+getgenv().Key = "kd2415278d2a026df7ecadb4"
+repeat wait() until game:IsLoaded()
+loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
